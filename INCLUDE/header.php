344109412eeb0e1955ba7771fd276aca9e88253d
eflaquet <edouard.flaquet@gmail.com>
2021-08-08 22:33:36 +0200

    update css

---
+++ INCLUDE/header.php
@@ -0,0 +1,35 @@
+<header>
+    <div class="baniere">
+
+    </div>
+    <div id="wrap">
+        <nav class="navbar">
+            <li><a href="#">Accueil</a></li>
+            <li>
+                <a href="forum/index.php">Forum</a>
+            </li>
+            <li>
+                <a href="#">..</a>
+
+            </li>
+            <li>
+                <a href="#">.</a>
+
+            </li>
+            <li>
+                <a href="#">.</a>
+
+            </li>
+            <li>
+                <a href="#">.</a>
+
+            </li>
+        </nav>
+    </div>
+    <div class="sous-nav">
+        <div class="server-connect">
+            NBConnecter : <spam id="online_players"></spam> / <?php echo $maxPlayers[0]['success']; ?>
+        </div>
+    </div>
+
+</header>

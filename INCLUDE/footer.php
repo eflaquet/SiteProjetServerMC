4b1d08c50f23c937787520d2c54961b53d83f2c5
eflaquet <edouard.flaquet@gmail.com>
2021-08-08 15:08:15 +0200

    update link

---
+++ INCLUDE/footer.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="CSS/footerstyle.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<footer>
+    <div class="sous-footer">
+        <div class="social"><a href="#"><i class="icon fab fa-twitter"></i></a><a href="#"><i class="icon fab fa-tiktok"></i></a><a href="#"><i class="icon fab fa-discord"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Accueil</a></li>
+            <li class="list-inline-item"><a href="#">..</a></li>
+            <li class="list-inline-item"><a href="#">..</a></li>
+            <li class="list-inline-item"><a href="#">..</a></li>
+            <li class="list-inline-item"><a href="#">..</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2021</p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -15,8 +15,8 @@ $api = new JSONAPI($ip, $port, $utilisateur, $motdepasse, $salt); // on affiche 
 //if (!$api->call("")) echo "non";
 if ($onlinePlayers = $api->call("players.online"))
     echo sizeof($onlinePlayers[0]['success']);
-    if($maxPlayers = $api->call("getPlayerLimit"))
-   print_r($api->call("getPlayerLimit"));
+if ($maxPlayers = $api->call("getPlayerLimit"))
+    print_r($api->call("getPlayerLimit"));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,8 +27,9 @@ if ($onlinePlayers = $api->call("players.online"))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accuiel</title>
     <link rel="stylesheet" href="CSS/cssindex.css">
-
+    <link rel="stylesheet" href="INCLUDE/CSS/footerstyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <script type="text/javascript" src="JS/API_source.js"></script>
 </head>
 
@@ -41,52 +42,41 @@ if ($onlinePlayers = $api->call("players.online"))
             <nav class="navbar">
                 <li><a href="#">Accueil</a></li>
                 <li>
-                    <a href="#">Forum</a>
+                    <a href="forum/index.php">Forum</a>
                 </li>
                 <li>
                     <a href="#">..</a>
-                    
+
                 </li>
                 <li>
                     <a href="#">.</a>
-                   
+
                 </li>
                 <li>
                     <a href="#">.</a>
-                   
+
                 </li>
                 <li>
                     <a href="#">.</a>
-                   
+
                 </li>
             </nav>
         </div>
         <div class="sous-nav">
             <div class="server-connect">
-                NBConnecter :  <spam id ="online_players"></spam>  /  <?php echo $maxPlayers[0]['success']; ?>
+                NBConnecter : <spam id="online_players"></spam> / <?php echo $maxPlayers[0]['success']; ?>
             </div>
         </div>
-        
+
     </header>
     <main>
         <section>
-        <div class="server-description">
-            eeee
-        </div>    </section>
+            <div class="server-description">
+                eeee
+            </div>
+        </section>
     </main>
-    <footer>
-        <div class="sous-footer">
-        <div class="social"><a href="#"><i class="icon fab fa-twitter"></i></a><a href="#"><i class="icon fab fa-tiktok"></i></a><a href="#"><i class="icon fab fa-discord"></i></a></div>
-        <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Accueil</a></li>
-                <li class="list-inline-item"><a href="#">..</a></li>
-                <li class="list-inline-item"><a href="#">..</a></li>
-                <li class="list-inline-item"><a href="#">..</a></li>
-                <li class="list-inline-item"><a href="#">..</a></li>
-            </ul>
-            <p class="copyright">Company Name © 2021</p>    
-        </div>
-    </footer>
+    <?php include 'INCLUDE/footer.php'; ?>
 </body>
 
 </html>
